3ad18c053bc03421d4dd1cfa5118030f63a4388c
Mushafiya <kmushafiya003@gmail.com>
2024-08-01 12:35:04 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="w-screen min-h-screen overflow-x-hidden bg-off-white-bg">
-       <div className='w-[500px] h-[500px] border border-red bg-white'>
-
-        hello
-
-       </div>
+    <div className="w-screen min-h-screen overflow-x-hidden ">
+       
     </div>
   );
 }
