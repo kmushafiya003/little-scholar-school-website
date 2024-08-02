41fe9c3e8104a8718d4c98d5dab7ac827365fca6
nalini jadhav <nalini@edulab.in>
2024-08-02 16:22:01 +0530

    herosection-nalini

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import HeroSection from './components/home/HeroSection';
 
 
 function App() {
   return (
     <div className="w-screen min-h-screen overflow-x-hidden bg-black">
-
+       <HeroSection />
 
     </div>
 
