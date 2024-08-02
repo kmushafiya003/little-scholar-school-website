f638b1f65a212f5065f5689b1d4b0e12bbdae89d
Mushafiya <kmushafiya003@gmail.com>
2024-08-02 17:13:42 +0530

    Completed Icon Section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-import Icon from './components/home/Icon';
+
 
 
 function App() {
   return (
     <div className="w-screen min-h-screen overflow-x-hidden">
 
- <Icon/>
+
  
 
     </div>
