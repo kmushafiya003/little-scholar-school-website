4ef6ba855e23071d3214cd2f09d47a99c7b7dab1
Ashique <ansariazad7864@gmail.com>
2024-08-02 15:38:02 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-
+import Message from './components/home/Message.jsx'
 import './App.css';
+
 
 
 function App() {
   return (
-    <div className="w-screen min-h-screen overflow-x-hidden bg-black">
-
-
+    <div className="w-screen min-h-screen overflow-x-hidden">
+      <Message />
     </div>
 
-
-
-
-  );
+ );
 }
 
 export default App;
