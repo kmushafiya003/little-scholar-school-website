e6a6b96a95a865d9a1f4b5843f075f9480313f1a
Ashique <ansariazad7864@gmail.com>
2024-08-03 13:24:12 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 
 import './App.css';
-import HeroSection from './components/home/HeroSection';
-import Navbar from './components/common/Navbar.jsx';
-
-
-
+import Gallery from './components/home/Gallery.jsx'
+import Message from './components/home/Message.jsx';
+import IconSection from './components/home/IconSection.jsx';
 
 function App() {
   return (
 
     <div className="w-screen min-h-screen overflow-x-hidden bg-black">
-      <Navbar/>
-       
+      <Gallery />
+      <Message />
+      <IconSection />
+      
     </div>
 
   );
