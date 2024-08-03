445d2ef5cc925d7db9b02e0476a135e90a6d49b1
Ashique <ansariazad7864@gmail.com>
2024-08-03 20:00:11 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-
 import './App.css';
-import Gallery from './components/home/Gallery.jsx'
-import Message from './components/home/Message.jsx';
-import IconSection from './components/home/IconSection.jsx';
-import Footer from './components/home/Footer.jsx';
 
 function App() {
   return (
 
-    <div className="w-screen min-h-screen overflow-x-hidden bg-black">
-      <Gallery />
-      <Message />
-      <IconSection />
-      <Footer />
+    <div className="w-screen min-h-screen overflow-x-hidden">
+     
     </div>
 
   );
