de5e01eeba692faf6e801a92ed78ca7333e61b95
pankaj-03 <pankajsunal045@gmail.com>
2024-08-09 15:51:15 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-
-
 import './App.css';
-<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom';
 // import AdmissionEnquiry from './components/adimission/AdmissionEnquiry';
-
 // import { Route, Routes } from 'react-router-dom';
 // import Home from './pages/Home';
 
->>>>>>> a2b3c93583afb5c6bf511f14572cfe505d7071de
 
 
 
@@ -26,12 +21,10 @@ const App = () => {
 
       </Routes>  */}
 
-<<<<<<< HEAD
+
   {/* <AdmissionEnquiry/> */}
-=======
 
 
->>>>>>> a2b3c93583afb5c6bf511f14572cfe505d7071de
     
     </div>
   );
