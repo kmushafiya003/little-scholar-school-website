05aa5f07b32eac0a93b9800675764edbd607778e
Mushafiya <kmushafiya003@gmail.com>
2024-08-10 20:13:18 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React from 'react';
 import './App.css';
 
-//import Infragallery from '../src/components/about/Infragallery'
-//import Infrastructure  from '../src/components/home/infrastructure'
-//import { Route, Routes } from 'react-router-dom';
-//import StudentForm from '../../src/components/adimission/Helpdesk'
-// import { Route, Routes } from 'react-router-dom';
-// import Home from './pages/Home';
-//import StudentForm from './components/adimission/Helpdesk';
 
 
-;
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+
 const App = () => {
   return (
 
 
     <div className="min-h-screen overflow-x-hidden ">
-       {/* <Routes>
+       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/founder-desk' element={<FounderDesk />} />
-        
 
-      </Routes>  */}
-      
+      </Routes> 
      
-      
-
-
-  {/* <AdmissionEnquiry/> */}
-  {/* < Infragallery />
-  < Infrastructure /> */}
-
  
 
-    
     </div>
   );
 }
