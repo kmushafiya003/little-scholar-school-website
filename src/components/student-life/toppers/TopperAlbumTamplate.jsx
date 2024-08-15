3f6ae8c698c20d7b9c5d611d822e416bfbe426a3
Ashique <ansariazad7864@gmail.com>
2024-08-15 13:34:17 +0530

    topper section

--- src/components/student-life/toppers/TopperAlbumTamplate.jsx
+++ src/components/student-life/toppers/TopperAlbumTamplate.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
-import { session_23_24 } from '../../../data/StudentLife/topper-data'
+// src/components/TopperAlbum/TopperAlbumTamplate.js
 
-function TopperAlbumTamplate({topperYear}) {
+import React from 'react';
+
+const tabData = [
+  {
+    name: "Class 'X'",
+    className: '10th Standard' // Corresponding description for filtering
+  },
+  {
+    name: "Class 'XII'",
+    className: '12th Standard' // Corresponding description for filtering
+  }
+];
+
+const TopperAlbumTamplate = ({ activeTab, session }) => {
+  const activeClass = tabData.find(tab => tab.name === activeTab)?.className;
+
   return (
-    <section className="relative w-full">
-      <div className="grid w-10/12 grid-cols-1 pt-20 mx-auto gap-y-6 gap-x-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-        {session_23_24.map((data, index) => (
-          <div key={index} className="flex flex-col items-center transition duration-300 transform border border-none rounded-3xl shadow1 hover:scale-105">
-            <img 
-              src={data.image} 
-              alt={data.title} 
-              className="object-cover w-full h-[100%] mb-4 border border-black rounded-lg"
-            />
-            <h1 className="mt-2 text-lg font-semibold text-center">{data.title}</h1>
-            <p className="mt-1 text-center">{data.percentage}</p>
-          </div>
-        ))}
+    <section className='relative w-full'>
+      <div className='w-10/12 mx-auto'>
+        <div className='grid grid-cols-1 pt-3 mx-auto gap-y-6 gap-x-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
+          {session
+            .filter(data => data.desc === activeClass)
+            .map((data, index) => (
+              <div
+                key={index}
+                className='flex flex-col items-center transition duration-300 transform border border-none rounded-3xl shadow1 hover:scale-105'
+              >
+                <img
+                  src={data.image}
+                  alt={data.title}
+                  className='object-cover w-full h-[100%] mb-4 border border-black rounded-lg'
+                />
+                <h1 className='mt-2 text-lg font-semibold text-center'>
+                  {data.title}
+                </h1>
+                <p className='mt-1 text-center'>{data.percentage}</p>
+              </div>
+            ))}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default TopperAlbumTamplate
+export default TopperAlbumTamplate;
