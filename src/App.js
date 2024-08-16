b23cb9b81389640fc1cd03f55ad1e6443143d44a
pankaj-03 <pankajsunal045@gmail.com>
2024-08-16 06:48:03 +0530

    Booklist

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import './index.css';
-
+import BookList from './components/MandatoryDisclosure/BookList';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import ScrollText from './components/common/ScrollText';
 import Loading from './components/common/Loading';
 import ScrollTop from './components/common/ScrollTop';
 
-const Home = lazy(() => import('./pages/Home'));
-const About = lazy(() => import('./pages/About'));
-const FounderDesk = lazy(() => import('./components/about/FounderDesk'));
-const PrincipalDesk = lazy(() => import('./components/about/PrincipalDesk'));
-const SchoolManagement = lazy(() => import('./components/about/SchoolManagement'));
-const ManagingCommittee = lazy(() => import('./components/about/ManagingCommittee'));
-const Principles = lazy(() => import('./components/about/Principles'));
-const Infragallery = lazy(() => import('./components/about/Infragallery'));
-const Admission = lazy(() => import('./pages/Admission'));
-const Helpdesk = lazy(() => import('./components/adimission/Helpdesk'));
-const AdmissionProcedure = lazy(() => import('./components/adimission/AdmissionProcedure'));
-const Registration = lazy(() => import('./components/adimission/Registration'));
-const FeeStructure = lazy(() => import('./components/adimission/FeeStructure'));
-const SchoolTiming = lazy(() => import('./components/adimission/SchoolTiming'));
-const FAQ = lazy(() => import('./components/adimission/FQA'));
+// const Home = lazy(() => import('./pages/Home'));
+// const About = lazy(() => import('./pages/About'));
+// const FounderDesk = lazy(() => import('./components/about/FounderDesk'));
+// const PrincipalDesk = lazy(() => import('./components/about/PrincipalDesk'));
+// const SchoolManagement = lazy(() => import('./components/about/SchoolManagement'));
+// const ManagingCommittee = lazy(() => import('./components/about/ManagingCommittee'));
+// const Principles = lazy(() => import('./components/about/Principles'));
+// const Infragallery = lazy(() => import('./components/about/Infragallery'));
+// const Admission = lazy(() => import('./pages/Admission'));
+// const Helpdesk = lazy(() => import('./components/adimission/Helpdesk'));
+// const AdmissionProcedure = lazy(() => import('./components/adimission/AdmissionProcedure'));
+// const Registration = lazy(() => import('./components/adimission/Registration'));
+// const FeeStructure = lazy(() => import('./components/adimission/FeeStructure'));
+// const SchoolTiming = lazy(() => import('./components/adimission/SchoolTiming'));
+// const FAQ = lazy(() => import('./components/adimission/FQA'));
 
 
 const App = () => {
   return (
     <div className="min-h-screen overflow-x-hidden">
-      <ScrollText />
+      {/* <ScrollText />
       <Navbar />
 
       <Suspense fallback={<div> <Loading/> </div>}>
@@ -56,7 +56,8 @@ const App = () => {
       </Suspense>
 
       <ScrollTop/>
-      <Footer />
+      <Footer /> */}
+     
     </div>
   );
 }
