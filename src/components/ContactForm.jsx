64d11cb708b4f769c4ad58606d1cd4386fbe16a2
Ashique <ansariazad7864@gmail.com>
2024-08-18 23:33:31 +0530

    summer camp form completed and also done contact us form done

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,123 +1,192 @@
-import React from 'react'
-import { BiSolidPhoneCall } from "react-icons/bi";
-import { MdEmail } from "react-icons/md";
-import { IoEarthOutline } from "react-icons/io5";
-import { socialIcon } from '../data/Contact/contact-data'
-
-
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BiSolidPhoneCall } from 'react-icons/bi';
+import { MdEmail } from 'react-icons/md';
+import { IoEarthOutline } from 'react-icons/io5';
+import { socialIcon } from '../data/Contact/contact-data';
 
 const ContactForm = () => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const [showForm, setShowForm] = useState(true);
+  const onSubmit = data => {
+    console.log(data);
+    toast.success('Message sent successfully!');
+    reset();
+    setShowForm(false);
 
-    const countryCode = "+91"
-    const phoneNumber = "9105666371"
-    const formattedNumber = `${countryCode} ${phoneNumber}`
+    setTimeout(() => {
+      setShowForm(true);
+    }, 100);
+  };
 
-    const emailAddress = "littlescholars.kashipur@gmail.com"
+  const countryCode = '+91';
+  const phoneNumber = '9105666371';
+  const formattedNumber = `${countryCode} ${phoneNumber}`;
 
-    const address = "Bhalla Farm Pratappur, Ramnagar Road, Kashipur-244713, Distt. U.S.Nagar"
-    const googleMapUrl = "https://www.google.com/maps/place/Little+Scholars+Kashipur/@29.260355,79.012721,18z/data=!4m6!3m5!1s0x390a4382a01af131:0x479bbb4c65a636f9!8m2!3d29.2602894!4d79.0127187!16s%2Fg%2F11spxf770n?hl=en&entry=ttu"
+  const emailAddress = 'littlescholars.kashipur@gmail.com';
 
+  const address = 'Bhalla Farm Pratappur, Ramnagar Road, Kashipur-244713, Distt. U.S.Nagar';
+  const googleMapUrl = 'https://www.google.com/maps/place/Little+Scholars+Kashipur/@29.260355,79.012721,18z/data=!4m6!3m5!1s0x390a4382a01af131:0x479bbb4c65a636f9!8m2!3d29.2602894!4d79.0127187!16s%2Fg%2F11spxf770n?hl=en&entry=ttu';
 
+  return (
+    <section className='bg-off-white-bg'>
+      {/* Container */}
+      <div className='w-10/12 py-20 mx-auto gap-y-12'>
+        {/* Top part (heading) */}
+        <div className='flex flex-col items-center gap-y-2'>
+          <h1 className='text-[40px] font-bold'>Contact Us</h1>
+          <p className='text-lg font-medium text-dark-grey'>You can get in touch with us here!</p>
+        </div>
 
-    return (
-        <section className='bg-off-white-bg'>
-
-            {/* -------------------- container ---------------- */}
-            <div className='w-10/12 mx-auto  flex flex-col gap-y-12 items-center py-20'>
-
-                {/* ----------top part (heading )----------- */}
-
-                <div className='flex flex-col gap-y-2 items-center'>
-                    <h1 className='text-[40px] font-bold'>Contact Us</h1>
-                    <p className='text-lg font-medium text-dark-grey'>You can get in touch with us here!</p>
-                </div>
-
-                {/* -------------- bottom part (form section)----------- */}
-
-                <div className='flex justify-between bg-white rounded-xl  w-full py-4 px-4'>
-
-                    {/* ---------- Contact  part --------- */}
-
-                    <div className=' flex flex-col gap-y-20  w-[42%] bg-bright-black text-white py-8 px-8 rounded-l-xl'>
-
-                        {/* --------- heading ------ */}
-
-                        <div className='flex flex-col gap-y-3'>
-
-                            <p className='text-xl'>Happy to help!</p>
-                            <h2 className='text-dark-light-black'>If you need someone to talk to, we listen. We won’t judge or tell you what to do.</h2>
-
-                        </div>
-
-
-                        {/* ---------- contact ways --------- */}
-                        <div className='flex flex-col gap-y-8'>
-
-                            {/* --------- phone ------- */}
-
-                            <a href={`tel:${countryCode}${phoneNumber}`} className='flex items-center gap-x-6 font-light group'>
-                                <span className='text-[24px] group-hover:rotate-[360deg] transition-all duration-500'><BiSolidPhoneCall /></span>
-                                <span>{formattedNumber}</span>
-                            </a>
-
-
-                            {/* ----------- email --------------- */}
-
-                            <a href={`mailto:${emailAddress}`} className='flex items-center gap-x-6 font-light group'>
-                                <span className='text-[24px] group-hover:rotate-[360deg] transition-all duration-500'><MdEmail /></span>
-                                <span>{emailAddress}</span>
-                            </a>
-
-                            {/* ---------------------- address ------------ */}
-
-                            <a className='flex items-center gap-x-6 font-light group' href={googleMapUrl} target='_blank' rel="noopener noreferrer" >
-                                <span className='text-[24px] group-hover:rotate-[360deg] transition-all duration-500'><IoEarthOutline /></span>
-                                <span>{address}</span>
-                            </a>
-
-
-                        </div>
-
-
-                        {/* --------- social media ----------- */}
-                        <div className='flex gap-x-8'>
-
-                            {
-                                socialIcon.map((item) => (
-
-                                    <a
-                                        href={item.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className='bg-dark-blue hover:bg-white transition-all duration-300 group w-[36px] h-[36px] grid place-items-center rounded-full'
-                                       
-                                    >
-                                        <span className='text-[20px] text-white group-hover:text-dark-blue ransition-all duration-300'>{item.icon}</span>
-                                    </a>
-
-
-                                ))
-                            }
-
-                        </div>
-
-                    </div>
-
-                    {/* --------------- form part ------------- */}
-
-                    <form className='w-[58%] border border-dark-blue'>
-
-
-                        {/* Yaha se tumko start krna hai @Ashique!!! */}
-
-                    </form>
-
-                </div>
-
+        {/* Bottom part (form section) */}
+        <div className='flex flex-col w-full gap-5 px-4 py-4 bg-white md:flex-row rounded-xl'>
+          {/* Contact part */}
+          <div className='flex flex-col gap-y-8 md:w-[42%] bg-bright-black text-white py-8 px-8 rounded-xl'>
+            {/* Heading */}
+            <div className='flex flex-col gap-y-3'>
+              <p className='text-xl'>Happy to help!</p>
+              <h2 className='text-dark-light-black'>
+                If you need someone to talk to, we listen. We won’t judge or tell you what to do.
+              </h2>
             </div>
 
-        </section>
-    )
-}
+            {/* Contact ways */}
+            <div className='flex flex-col gap-y-8'>
+              {/* Phone */}
+              <a href={`tel:${countryCode}${phoneNumber}`} className='flex items-center font-light gap-x-6 group'>
+                <span className='text-[24px] group-hover:rotate-[360deg] transition-all duration-500'>
+                  <BiSolidPhoneCall />
+                </span>
+                <span>{formattedNumber}</span>
+              </a>
 
-export default ContactForm
+              {/* Email */}
+              <a href={`mailto:${emailAddress}`} className='flex items-center font-light gap-x-6 group'>
+                <span className='text-[24px] group-hover:rotate-[360deg] transition-all duration-500'>
+                  <MdEmail />
+                </span>
+                <span>{emailAddress}</span>
+              </a>
+
+              {/* Address */}
+              <a className='flex items-center font-light gap-x-6 group' href={googleMapUrl} target='_blank' rel='noopener noreferrer'>
+                <span className='text-[24px] group-hover:rotate-[360deg] transition-all duration-500'>
+                  <IoEarthOutline />
+                </span>
+                <span>{address}</span>
+              </a>
+            </div>
+
+            {/* Social media */}
+            <div className='flex gap-x-8'>
+              {socialIcon.map((item, index) => (
+                <a key={index} href={item.link} target='_blank' rel='noopener noreferrer' className='bg-dark-blue hover:bg-white transition-all duration-300 group w-[36px] h-[36px] grid place-items-center rounded-full'>
+                  <span className='text-[20px] text-white group-hover:text-dark-blue transition-all duration-300'>{item.icon}</span>
+                </a>
+              ))}
+            </div>
+          </div>
+
+          {/* Form part */}
+          {showForm && (
+            <div className='w-full p-10 md:w-[58%]'>
+              <form onSubmit={handleSubmit(onSubmit)} className='w-full rounded'>
+                <div className='grid grid-cols-1 gap-2 md:grid-cols-2'>
+                  <div className='mb-4'>
+                    <label className='block text-gray-700'>First Name</label>
+                    <input
+                      {...register('firstName', { required: true })}
+                      className='w-full p-2 mt-1 border-b-2 rounded'
+                      placeholder='First Name'
+                    />
+                    {errors.firstName && <span className='text-red-500'>First Name is required</span>}
+                  </div>
+                  <div className='mb-4'>
+                    <label className='block text-gray-700'>Last Name</label>
+                    <input
+                      {...register('lastName', { required: true })}
+                      className='w-full p-2 mt-1 border-b-2 rounded'
+                      placeholder='Last Name'
+                    />
+                    {errors.lastName && <span className='text-red-500'>Last Name is required</span>}
+                  </div>
+                  <div className='mb-4'>
+                    <label className='block text-gray-700'>Email</label>
+                    <input
+                      {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+                      className='w-full p-2 mt-1 border-b-2 rounded'
+                      placeholder='Email'
+                    />
+                    {errors.email && <span className='text-red-500'>Valid Email is required</span>}
+                  </div>
+                  <div className='mb-4'>
+                    <label className='block text-gray-700'>Phone Number</label>
+                    <input
+                      {...register('phoneNumber', { required: true, pattern: /^[0-9+\-() ]+$/ })}
+                      className='w-full p-2 mt-1 border-b-2 rounded'
+                      placeholder='Phone Number'
+                    />
+                    {errors.phoneNumber && <span className='text-red-500'>Valid Phone Number is required</span>}
+                  </div>
+                  <div>
+                    <label className='text-gray-700'>Select Subject?</label>
+                    <div className='mt-1 space-x-1'>
+                      <label className='inline-flex items-center'>
+                        <input
+                          {...register('subject', { required: true })}
+                          type='radio'
+                          value='General Inquiry'
+                          className='form-radio'
+                        />
+                        <span className='ml-2'>General Inquiry</span>
+                      </label>
+                      <label className='inline-flex items-center'>
+                        <input
+                          {...register('subject', { required: true })}
+                          type='radio'
+                          value='Support'
+                          className='form-radio'
+                        />
+                        <span className='ml-2'>Support</span>
+                      </label>
+                      <label className='inline-flex items-center'>
+                        <input
+                          {...register('subject', { required: true })}
+                          type='radio'
+                          value='Feedback'
+                          className='form-radio'
+                        />
+                        <span className='ml-2'>Feedback</span>
+                      </label>
+                    </div>
+                    {errors.subject && <span className='text-red-500'>Subject is required</span>}
+                  </div>
+                </div>
+                <div className='mb-4'>
+                  <label className='block text-gray-700'>Message</label>
+                  <textarea
+                    {...register('message', { required: true })}
+                    className='w-full p-2 mt-1 border-b-2 rounded'
+                    placeholder='Write your message...'
+                  />
+                  {errors.message && <span className='text-red-500'>Message is required</span>}
+                </div>
+                <button
+                  type='submit'
+                  className='w-full px-12 py-4 font-medium leading-4 text-white border border-transparent rounded-full shadow-sm bg-dark-blue md:w-auto hover:bg-white hover:text-black hover:border-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                >
+                  Send Message
+                </button>
+              </form>
+            </div>
+          )}
+        </div>
+      </div>
+      <ToastContainer />
+    </section>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/news&calendar/student/SummerCamp2.jsx
@@ -0,0 +1,246 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { Fieldset } from '@headlessui/react'
+import { Field } from '@headlessui/react'
+import swim from '../../../images/News&Calendar/swim.webp'
+
+const SummerCampConsentForm2 = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm()
+  const [showForm, setShowForm] = useState(true)
+
+  const onSubmit = data => {
+    console.log(data)
+    toast.success('Form submitted successfully!')
+    reset()
+    setShowForm(false)
+  }
+
+  const handleAnotherForm = () => {
+    setShowForm(true)
+  }
+
+  return (
+    <section className='relative w-full'>
+      <div className='grid w-10/12 grid-cols-1 gap-20 mx-auto xxl:grid-cols-2'>
+        <img src={swim} alt='Summer Camp' />
+        <div className='max-w-2xl p-6 mx-auto border-[3.5px] rounded-lg shadow-md'>
+          <div className='p-10 border-[3.5px] border-black rounded-md pt-10'>
+            <h2 className='mb-1 font-semibold text-[1.5rem]'>
+              SUMMER CAMP CONSENT
+            </h2>
+            <div className='p-5 '>
+              <ul className='list-disc'>
+                <li>Timings: 7:00 am - 10:00 am</li>
+                <li>Fee for registration: ₹2000 per head</li>
+                <li>Scheduled between: 1-15 June</li>
+              </ul>
+            </div>
+
+            {showForm ? (
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='grid grid-cols-1 gap-12 mb-10 md:grid-cols-2'>
+                  <div>
+                    <input
+                      type='text'
+                      placeholder='Enter Student Name'
+                      {...register('studentName', { required: true })}
+                      className='w-full px-4 py-3 border border-gray-300 rounded-md'
+                    />
+                    {errors.studentName && (
+                      <p className='text-sm text-red-600'>
+                        Student name is required
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <input
+                      type='text'
+                      placeholder='Enter Parent Name'
+                      {...register('parentName', { required: true })}
+                      className='w-full px-4 py-3 border border-gray-300 rounded-md'
+                    />
+                    {errors.parentName && (
+                      <p className='text-sm text-red-600'>
+                        Parent name is required
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className='w-full mb-8 border-gray-300 '>
+                  <div>
+                    <input
+                      type='text'
+                      placeholder='Students contact no.'
+                      {...register('contactNo', {
+                        required: true,
+                        pattern: /^[0-9]{10}$/
+                      })}
+                      className='w-full px-4 py-3 border border-gray-300 rounded-md'
+                    />
+                    {errors.contactNo && (
+                      <p className='text-sm text-red-600'>
+                        Contact number is required and should be 10 digits
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div>
+                  <div className='mb-3'>
+                    <h2 className='text-lg'>
+                      Select Activities (maximum 2 activities/clubs):
+                    </h2>
+                  </div>
+                  <div className='grid grid-cols-2 mb-4'>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activitySwimming')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Swimming</label>
+                    </div>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activityBadmintonYoga')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Badminton / Yoga</label>
+                    </div>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activityCricket')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Cricket</label>
+                    </div>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activityBasketball')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Basketball</label>
+                    </div>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activityVolleyball')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Volleyball</label>
+                    </div>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activityChess')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Chess</label>
+                    </div>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activityFineArtsPainting')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Fine Arts / Painting</label>
+                    </div>
+                    <div className='flex items-center mb-2'>
+                      <input
+                        type='checkbox'
+                        {...register('activityCookery')}
+                        className='mr-2'
+                      />
+                      <label className='text-sm'>Cookery</label>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className='mb-2'>
+                    <h2 className='text-[1.1rem]'>
+                      Kindly deposit ₹2000/- for registration on the bank
+                      details given below:
+                    </h2>
+                    <ul className='p-4 mr-2 text-sm list-disc'>
+                      <li>AC NO:- 0262002100034868</li>
+                      <li>IFSC:- PUNB0026200</li>
+                    </ul>
+                    <h2 className='text-[1.1rem] mb-2'>
+                      Kindly upload a screenshot image of the transaction
+                      details here:
+                    </h2>
+                    <Fieldset>
+                      <Field label='Picture' error={errors.picture}>
+                        <input
+                          {...register('picture', {
+                            required: 'Transaction screenshot is required'
+                          })}
+                          type='file'
+                          id='picture'
+                          className='w-full px-3 py-2 border border-gray-300 rounded-md'
+                        />
+                      </Field>
+                    </Fieldset>
+                  </div>
+                </div>
+                <div className='flex items-center mb-6'>
+                  <input
+                    type='checkbox'
+                    {...register('consent', { required: true })}
+                    className='mr-2'
+                  />
+                  <label className='text-sm text-justify'>
+                    By submitting this form, I acknowledge that I have read and
+                    agree to the terms and conditions of the event/activity. I
+                    also consent to any necessary safety measures and
+                    guidelines.
+                  </label>
+                </div>
+                {errors.consent && (
+                  <p className='text-sm text-red-600'>
+                    You must agree before submitting
+                  </p>
+                )}
+                <div className='flex pt-4'>
+                  <button
+                    type='submit'
+                    className='w-full px-12 py-4 text-sm font-medium leading-4 text-white border border-transparent rounded-full shadow-sm bg-dark-blue md:w-auto hover:bg-white hover:text-black hover:border-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  >
+                    Submit Now
+                  </button>
+                </div>
+              </form>
+            ) : (
+              <div className='text-center'>
+                <h2 className='text-xl font-semibold text-green-600'>
+                  Thank you for submitting the form!
+                </h2>
+                <p className='mt-2 text-gray-700'>
+                  We have received your submission.
+                </p>
+                <button
+                  onClick={handleAnotherForm}
+                  className='px-12 py-4 mt-4 text-sm font-medium leading-4 text-white border border-transparent rounded-full shadow-sm bg-dark-blue hover:bg-white hover:text-black hover:border-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                >
+                  Fill another form
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+    </section>
+  )
+}
+
+export default SummerCampConsentForm2
